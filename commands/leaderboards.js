a3d639f7aa06b8bd1e97c04d90f814cea6357d36
Glitch (hatchverse) <none>
2019-06-24 23:01:52 +0000

    🏓🐚 Checkpoint ./commands/leaderboards.js:3046037/608:3089165/172

--- commands/leaderboards.js
+++ commands/leaderboards.js
@@ -10,12 +10,20 @@ module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
   var type = args[0].toLowerCase()
   if (type == "gems" || type ==  "gem" || type ==  "money" || type ==  "$") {
-    db.all("SELECT Tag FROM Users ORDER BY Gems DESC;", (err, items) => {
+    db.all("SELECT * FROM Users ORDER BY Gems DESC;", (err, items) => {
       console.log((items))
     })
   } else {
-    db.all("SELECT Eggs FROM Users ORDER BY Eggs DESC LIMIT 25;", (err, items) => {
-      message.channel.send(String(items))
+    db.all("SELECT * FROM Users ORDER BY Eggs DESC LIMIT 25;", (err, items) => {
+      let i = 1;
+      
+      let embed = new Discord.RichEmbed()
+      .setAuthor('Hatchverse Leaderboard (Eggs Opened)')
+      .setDescription(items.map(item => `${i++}. ${bot.fetchUser(item.Tag)} <:Beginner_Egg:592440252979871745> ${item.Eggs}`))
+      .setTimestamp()
+      .setFooter(bot.user.username, bot.user.displayAvatarURL)
+      
+      message.channel.send(embed)
     })
   }
 
