69ab99bb03128c797cac233634c64cf46c70b19d
Glitch (hatchverse) <none>
2019-06-24 23:23:51 +0000

    💖👳 Checkpoint ./commands/leaderboards.js:3046037/3475:3089165/1207

--- commands/leaderboards.js
+++ commands/leaderboards.js
@@ -8,26 +8,33 @@ const db = new sqlite3.Database(dbFile);
 
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
-  var type = args[0].toLowerCase()
-  if (type == "gems" || type ==  "gem" || type ==  "money" || type ==  "$") {
+  if (args[0].toLowerCase() == 'gems') {
     db.all("SELECT * FROM Users ORDER BY Gems DESC;", (err, items) => {
-      console.log((items))
-    })
-  } else {
-    db.all("SELECT * FROM Users ORDER BY Eggs DESC LIMIT 25;", (err, items) => {
       let i = 1;
-      console.log(bot.members.get("302599378332549121"));
-      
+
       let embed = new Discord.RichEmbed()
-      .setAuthor('Hatchverse Leaderboard (Eggs Opened)')
-      .setDescription(items.map(item => `${i++}. ${bot.members.get(item.Tag)} <:Beginner_Egg:592440252979871745> ${item.Eggs}`))
+      .setAuthor('Hatchverse Leaderboard (Gems)')
+      .setDescription(items.map(item => `${i++}. **${bot.users.get(item.Tag).username + "#" + bot.users.get(item.Tag).discriminator}** :egg: **${item.Eggs}**`))
       .setTimestamp()
       .setFooter(bot.user.username, bot.user.displayAvatarURL)
-      
+
       message.channel.send(embed)
     })
   }
   
+  if(!args[0]) {
+      db.all("SELECT * FROM Users ORDER BY Eggs DESC LIMIT 25;", (err, items) => {
+      let i = 1;
+
+      let embed = new Discord.RichEmbed()
+      .setAuthor('Hatchverse Leaderboard (Eggs Opened)')
+      .setDescription(items.map(item => `${i++}. **${bot.users.get(item.Tag).username + "#" + bot.users.get(item.Tag).discriminator}** :egg: **${item.Eggs}**`))
+      .setTimestamp()
+      .setFooter(bot.user.username, bot.user.displayAvatarURL)
+
+      message.channel.send(embed)
+    })
+  }
 }
 
 module.exports.help = {
