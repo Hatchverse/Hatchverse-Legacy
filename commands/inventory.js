ea00362789c35726c1e8ae0f3a58fc55fee037a2
Glitch (hello-express) <none>
2019-06-24 19:08:11 +0000

    🎑👕 Checkpoint ./commands/inventory.js:3046037/704:3089165/112 ./modules/egg.js:3046037/1018:3089165/44 ./index.js:3089165/2 ./modules/addpet.js:3046037/842

--- commands/inventory.js
+++ commands/inventory.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
 const fs = module.require("fs")
-const dbFile = './.data/hatchverse8.db';
+const dbFile = global.db;
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(dbFile);
@@ -9,6 +9,18 @@ const db = new sqlite3.Database(dbFile);
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
   
+  db.all(`SELECT Inventory FROM Users WHERE Tag = ${message.author.id}`, (err, items) => {
+    let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.username}'s Inventory`, message.author.displayAvatarURL)
+    .setDescription(items[0].Inventory.split(', ').join(" "))
+    .setFooter(bot.user.username)
+    .setTimestamp()
+  
+    
+    message.channel.send(embed)
+  })
+  
+
 }
 
 module.exports.help = {
