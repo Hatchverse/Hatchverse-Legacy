ddd22ba4050b1d1d989145fe79393edf3e0b27bd
Glitch (hatchverse) <none>
2019-06-24 22:51:51 +0000

    🐏🕶 Checkpoint ./commands/leaderboards.js:3089165/571 ./commands/inventory.js:3046037/6 ./index.js:3046037/2:3089165/30 ./commands/js.js:3089165/13

--- commands/leaderboards.js
+++ commands/leaderboards.js
@@ -9,13 +9,13 @@ const db = new sqlite3.Database(dbFile);
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
   var type = args[0].toLowerCase()
-  if (type == "gems" || "gem" || "money" || "$") {
-    db.all("SELECT Tag FROM Users ORDER BY Gems ASC LIMIT 25", (err, items) => {
+  if (type == "gems" || type ==  "gem" || type ==  "money" || type ==  "$") {
+    db.all("SELECT Tag FROM Users ORDER BY Gems DESC;", (err, items) => {
       console.log((items))
     })
   } else {
-    db.all("SELECT Eggs FROM Users ORDER BY Eggs ASC LIMIT 25", (err, items) => {
-      console.log((items))
+    db.all("SELECT Eggs FROM Users ORDER BY Eggs DESC LIMIT 25;", (err, items) => {
+      message.channel.send(String(items))
     })
   }
 
