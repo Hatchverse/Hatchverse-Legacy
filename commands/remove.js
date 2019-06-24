a8548b2951743c8b1f6613274f97ad29e3016590
Glitch (hatchverse) <none>
2019-06-24 21:06:11 +0000

    🐑👢 Checkpoint ./commands/open.js:3046037/104 ./commands/remove.js:3046037/75

--- commands/remove.js
+++ commands/remove.js
@@ -10,8 +10,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
   
   db.all(`SELECT Inventory FROM Users WHERE Tag = '${message.author.id}'`, (err, items) => {
-    var type = args[0]
-    if (type == "all" || "All") {
+    if (args[0].toLowerCase() == 'all') {
       db.run("UPDATE Users SET Inventory = '' WHERE Tag = ?", message.author.id)
       message.channel.send(`Successfully removed **${items[0].Inventory.split(', ').length - 1}** pets!`)
     }
