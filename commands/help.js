f4bac6e18598978970710192f079516dfd519c13
Glitch (hatchverse) <none>
2019-06-25 21:08:39 +0000

    ❤️⛺️ Checkpoint ./commands/info.js:3046037/1034 ./commands/help.js:3046037/1449

---
+++ commands/help.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const fs = module.require("fs")
+const dbFile = global.db;
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(dbFile);
+
+module.exports.run = async (bot, message, args) => {
+  if(!message.content.startsWith(config.prefix)) return;
+  
+  let embed = new Discord.RichEmbed()
+  .setAuthor('Help/Commands', bot.user.displayAvatarURL) //aaa noima add it to the descript
+  .setDescription('`()open <egg name>`')
+  .addField('()eggs', '`()`')
+  .setColor('#9c13f7')
+  .setFooter(bot.user.username)
+  .setTimestamp()
+  
+  message.channel.send(embed)
+}
+
+module.exports.help = {
+  name: "help"
+}
--- commands/info.js
+++ commands/info.js
@@ -11,7 +11,9 @@ module.exports.run = async (bot, message, args) => {
   
   let embed = new Discord.RichEmbed()
   .setAuthor('Info', bot.user.displayAvatarURL) //aaa noima add it to the descript
-  .setDescription 
+  .setDescription('Hatchverse is a unique bot with an ability to open virtual eggs for pets that can be traded with a trade system or managed with an inventory system. You can also buy perks and boosts from the shop to help you on your egg opening adventure. Inspired by Rumble Studios Bubble Gum Simulator.')
+  .addField('Developers', `Krxnky, Syntax, Jullian, Exists, and Epoch`)
+  .addField('Credits', `Credits to Rumble Studios for pet/egg images`)
   .setColor('#9c13f7')
   .setFooter(bot.user.username)
   .setTimestamp()
@@ -20,5 +22,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-  name: "shop"
+  name: "info"
 }
