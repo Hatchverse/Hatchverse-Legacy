3c6a34859636dce213d22b815367bd63f4e9677f
Glitch (hatchverse) <none>
2019-06-25 19:54:38 +0000

    🐢👼 Checkpoint ./commands/egglist.js:3046037/252:4757885/93

--- commands/egglist.js
+++ commands/egglist.js
@@ -10,19 +10,19 @@ module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
   
   db.all(`SELECT * FROM Users WHERE Tag = '${message.author.id}'`, (err, items) => {
-    const eggs = parseInt(items[0].Gems);
-    const beginneregg = (eggs >= 0) ? ':unlock:' : ':lock:';
-    const spottedegg = (eggs >= 25) ? ':unclok:' : ':lock:';
-    const iceshardegg = (eggs >= 75) ? ':unlock:' : ':lock:';
-    const spikeyegg = (eggs >= 135) ? ':unlock:' : ':lock:';
-    const slimeyegg = (eggs >= 200) ? ':unlock:' : ':lock:';
-    const rainbowegg = (eggs >= 300) ? ':unlock:' : ':lock:';
-    const hackweekegg = (eggs >= 50) ? ':unlock:' : ':lock:'//go ahead and add the un added eggs so we dont have to do it later
+    const eggs = parseInt(items[0].Eggs);
+    const beginneregg = eggs >= 0 ? ':unlock:' : ':lock:';
+    const spottedegg = eggs >= 25 ? ':unlock:' : ':lock:';
+    const iceshardegg = eggs >= 75 ? ':unlock:' : ':lock:';
+    const spikeyegg = eggs >= 135 ? ':unlock:' : ':lock:';
+    const slimeyegg = eggs >= 200 ? ':unlock:' : ':lock:';
+    const rainbowegg = eggs >= 300 ? ':unlock:' : ':lock:';
+    const hackweekegg = eggs >= 50 ? ':unlock:' : ':lock:';//go ahead and add the un added eggs so we dont have to do it later
     
     let embed = new Discord.RichEmbed()
     .setAuthor('Eggs', bot.user.displayAvatarURL)
     .setColor('#9c13f7')
-    .setDescription(`${beginneregg} - <:Beginner_Egg:592440252979871745> Beginner Egg - 0 eggs\n${spottedegg} - <:Spotted_Egg:592446342228606976> Spotted Egg - 25 eggs`)
+    .setDescription(`${beginneregg}-<:Beginner_Egg:592440252979871745> Beginner Egg - :egg: **0**\n${spottedegg}-<:Spotted_Egg:592446342228606976> Spotted Egg - :egg: **25**\n${iceshardegg}-<:Ice_Shard_Egg:592447966023778325> Ice Shard Egg - :egg:**75**\n`)
     .setFooter(bot.user.username)
     .setTimestamp()
     
