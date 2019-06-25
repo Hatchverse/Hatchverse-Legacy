a02c0d4582db170576c6486392a9de73b624f934
Glitch (hatchverse) <none>
2019-06-25 19:43:38 +0000

    🍙🚈 Checkpoint ./commands/egglist.js:3046037/2427:4757885/480

---
+++ commands/egglist.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const fs = module.require("fs")
+const dbFile = global.db;
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(dbFile);
+
+module.exports.run = async (bot, message, args) => {
+  if(!message.content.startsWith(config.prefix)) return;
+  
+  db.all(`SELECT * FROM Users WHERE Tag = '${message.author.id}'`, (err, items) => {
+    const eggs = parseInt(items[0].Gems);
+    const beginneregg = (eggs >= 0) ? ':unlock:' : ':lock:';
+    const spottedegg = (eggs >= 25) ? ':unclok:' : ':lock:';
+    const iceshardegg = (eggs >= 75) ? ':unlock:' : ':lock:';
+    const spikeyegg = (eggs >= 135) ? ':unlock:' : ':lock:';
+    const slimeyegg = (eggs >= 200) ? ':unlock:' : ':lock:';
+    const rainbowegg = (eggs >= 300) ? ':unlock:' : ':lock:';
+    const hackweekegg = (eggs >= 50) ? ':unlock:' : ':lock:'//go ahead and add the un added eggs so we dont have to do it later
+    
+    let embed = new Discord.RichEmbed()
+    .setAuthor('Eggs', bot.user.displayAvatarURL)
+    .setColor('#9c13f7')
+    .setDescription(`${beginneregg} - <:Beginner_Egg:592440252979871745> Beginner Egg - 0 eggs\n${spottedegg} - <:Spotted_Egg:592446342228606976> Spotted Egg - 25 eggs`)
+    .setFooter(bot.user.username)
+    .setTimestamp()
+    
+    message.channel.send(embed)
+    
+  })
+}
+
+module.exports.help = {
+  name: "eggs"
+}
