7f29bb0262efe0a54232d1d42cf3b222074b9dd7
Glitch (hatchverse) <none>
2019-06-25 21:40:40 +0000

    ❤️⏱ Checkpoint ./commands/info.js:3046037/114 ./commands/help.js:3046037/14

--- commands/info.js
+++ commands/info.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
   .setAuthor('Info', bot.user.displayAvatarURL) //aaa noima add it to the descript
   .setDescription('Hatchverse is a unique bot with an ability to open virtual eggs for pets that can be traded with a trade system or managed with an inventory system. You can also buy perks and boosts from the shop to help you on your egg opening adventure. Inspired by Rumble Studios Bubble Gum Simulator.')
   .addField('Setup', 'To begin your egg opening journey, simply open the Beginner Egg. To do so use the open command with the egg name as `Beginner Egg`, ex. `()open Beginner Egg`')
-  .addField('Developers', `Krxnky, Syntax, Jullian, Exists, and Epoch`)
+  .addField('Developers', '@Krxnky#1824, @Epoch#1558, @Jullian#0001, @Syntax#1588, @Exists#9999')
   .addField('Credits', `Credits to Rumble Studios for pet/egg images`)
   .setColor('#9c13f7')
   .setFooter(bot.user.username)
