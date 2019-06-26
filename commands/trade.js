d6d5693af41de7348ebe346ff347e3dc8810408e
Glitch (hatchverse) <none>
2019-06-26 01:39:01 +0000

    🚜🎖 Checkpoint ./commands/trade.js:3046037/1540:4757885/29

--- commands/trade.js
+++ commands/trade.js
@@ -17,17 +17,50 @@ module.exports.run = async (bot, message, args) => {
       const receiverInv = receiver[0].Inventory.split(', ');
       
       const sendPet = args.slice(1).join('_');
-      const receivePet = args.slice(2).join('_');
+      let receivePet;
+    
+      const sendReg = new RegExp(sendPet);
+      const receiveReg = new RegExp(receivePet)
+      const senderOwn = senderInv.filter(pet => pet.match(sendReg))
       
-      const reg = new RegExp(pet);
-      const senderOwn = senderInv.filter(pet => pet.match(reg))
-      const receiverOwn = receiverInv.filter(pet => pet.match(reg))
+      if(senderOwn.length == 0) return message.channel.send(`You don't own **${sendPet}**`);
       
-      if(senderOwn.length == 0) return message.channel.send(`You don't own **${pet}**`);
-      if(receiverOwn.length == 0) return message.channel.send(`${mention} does not own ${}`)
+      message.channel.send('You receive?').then(() => {
+        message.channel.awaitMessages(response => message.content, { max: 1, time: 10000, erros: ['time'] })
+          .then((collected) => {
+            
+          })
+        })
+      })
+      
+//       const sendPet = args.slice(1).join('_');
+//       const receivePet = args.slice(1).remove(sendPet);
+      
+//       console.log(sendPet)
+//       console.log(receivePet)
+      
+//       const sendReg = new RegExp(sendPet);
+//       const receiveReg = new RegExp(receivePet);
+//       const senderOwn = senderInv.filter(pet => pet.match(sendReg))
+//       const receiverOwn = receiverInv.filter(pet => pet.match(receiveReg))
+      
+//       if(senderOwn.length == 0) return message.channel.send(`You don't own **${sendPet}**`);
+//       if(receiverOwn.length == 0) return message.channel.send(`${mention} does not own ${receivePet}`)
     })
   })
 }
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
 module.exports.help = {
   name: "trade"
 }
