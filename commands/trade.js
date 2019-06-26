a220aaee87c0127a50c48aea9ad6e211ef5ccbd6
Glitch (hatchverse) <none>
2019-06-26 00:24:00 +0000

    🏡🎳 Checkpoint ./commands/trade.js:3046037/2307:3089165/396

--- commands/trade.js
+++ commands/trade.js
@@ -9,20 +9,7 @@ const db = new sqlite3.Database(dbFile);
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
   
-  let incomingtrade = new Discord.RichEmbed()
-  .setAuthor('Trade Request', message.author.displayAvatarURL)
-  .setDescription(`Incoming Trade Request from \`${message.author.tag}\``)
-  .addField('You give', '<:Rainbow_Dogcat:592426717285449788>', true)
-  .addField('You receive', '<:Blurple_Doggy:591827146649894913>', true)
-  .setColor('#9c13f7')
-  .setFooter(bot.user.username)
-  .setTimestamp()
-  
-  message.channel.send(incomingtrade)
-  
-  let senttraderequest
 }
-
 module.exports.help = {
   name: "trade"
 }
