d71f94201c26e24579df2df3a98a45acd37dc4f7
Glitch (hatchverse) <none>
2019-06-26 00:34:59 +0000

    👁🚔 Checkpoint ./commands/trade.js:3046037/1619:3089165/1283

--- commands/trade.js
+++ commands/trade.js
@@ -8,7 +8,17 @@ const db = new sqlite3.Database(dbFile);
 
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
-  
+  if(!message.mentions.first()) return message.channel.send('Please mention a **user**!');
+  const mention = message.mentions.first();
+  db.all(`SELECT Inventory FROM User WHERE Tag = '${message.author.id}'`, (err, sender) => {
+    db.all(`SELECT Inventory FROM User WHERE Tag = '${mention.id}'`, (err, receiver) => {
+    // ()trade @whatehrerh :doggy: :kitty:
+      const senderInv = sender[0].Inventory.split(', ');
+      const receiverInv = sender[0].Inventory.split(', ');
+      console.log(senderInv)
+      console.log(receiverInv)
+    })
+  })
 }
 module.exports.help = {
   name: "trade"
