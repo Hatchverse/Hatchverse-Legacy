03c91cfac6f9d36a3905b6471a1daf658412c97e
Glitch (hatchverse) <none>
2019-07-15 16:05:58 +0000

    👞🌓 Checkpoint ./commands/dashboard.js:3089165/4582 ./aaacommands/dashboard.js:3089165/4713

---
+++ commands/dashboard.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const fs = module.require("fs")
+const dbFile = global.db;
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(dbFile);
+
+
+module.exports.run = async (bot, message, args) => {
+  if(!message.content.startsWith(config.prefix)) return;
+  if (!message.member.hasPermission("ADMINISTRATOR")) return;
+  var key = key(36)
+  var servername = message.guild.name
+  var serverid = message.guild.id
+  console.log(servername + " | " + serverid + " | " + key)
+  function key(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+}
+
+module.exports.help = {
+  name: "dashboard"
+}
