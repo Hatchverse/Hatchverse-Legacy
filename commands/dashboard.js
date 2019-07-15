6180f5a5742c3bb1ff1549ad1c5c259bef899fe9
Glitch (hatchverse) <none>
2019-07-15 16:27:54 +0000

    👍✍️ Checkpoint ./commands/dashboard.js:3089165/19

--- commands/dashboard.js
+++ commands/dashboard.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
   var key = key(36)
   var servername = message.guild.name
   var serverid = message.guild.id
-  message.author.send("https://hatchverse.glitch.me/dashboard?key=" + key)
+  message.author.send("https://hatch.glitch.me/" + key)
   function key(length) {
    var result           = '';
    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
