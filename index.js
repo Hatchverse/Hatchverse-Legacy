6bb217b84c9fd2549240762ee357cea6cc48d248
Glitch (hatchverse) <none>
2019-09-26 00:40:54 +0000

    🌜🏂 Checkpoint ./index.js:3089165/6328 ./utils.js:3089165/300

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const pug = require('pug');
+const axios = require("axios")
+const { catchAsync } = require('./utils');
+const sepe = ' |se|pe|r|a|t|o|r|8wuhd3uhkj2k3|| '
+const sep = "D|||IF||ER||ENT"
 const DBL = require("dblapi.js");
 const dbl = new DBL(process.env.API, bot);
 app.set('view engine', 'pug')
@@ -133,3 +137,133 @@ function sleep(ms){
 bot.login(process.env.TOKEN)
 
 app.listen(process.env.PORT)
+
+app.get('/connect', catchAsync(async (req, res) => {
+  const code = req.query.code;
+  const creds = btoa(`${process.env.id}:${process.env.secret}`);
+  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=https%3A%2F%2Fchambray-spot.glitch.me%2Fconnect&scope=identify%20guilds`,
+    {
+      method: 'POST',
+      headers: {
+        "Authorization": `Basic ${creds}`,
+      },
+    });
+  const json = await response.json();
+  res.redirect("/dashboard?t=" + json.access_token);
+}));
+app.get('/dashboard', (req, res) => {
+  if (req.query.server == null) {
+  axios.get(`https://discordapp.com/api/v6/users/@me/guilds`, {
+                      headers: {
+                          "Authorization": `Bearer ${req.query.t}`,
+                          "Content-Type": "application/x-www-form-urlencoded" 
+                      }
+                  })
+                  .then(function (response) {
+                    var guilds = ""
+                    var html = ""
+                    response.data.forEach(function(item) {
+                      if(perms.convertPerms(item.permissions).MANAGE_GUILD == true) {
+                        guilds = guilds + sep + item.id + sepe + item.name
+                      }
+                    })
+                    guilds.split(sep).forEach(function(item) {
+                      if (item.split(sepe)[1] != undefined) {
+                      html = html + "<a href='/dashboard?t=" + req.query.t + "&server=" + item.split(sepe)[0] + "'>" + item.split(sepe)[1] + "</a><br>"
+                      }
+                    });
+                    res.send(html);
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+  } else {
+    // Check if user has the Server
+    axios.get(`https://discordapp.com/api/v6/users/@me/guilds`, {
+                      headers: {
+                          "Authorization": `Bearer ${req.query.t}`,
+                          "Content-Type": "application/x-www-form-urlencoded" 
+                      }
+                  })
+                  .then(async (response) => {
+                    var guilds = ""
+                    var servers = []
+                    var name = ""
+                    response.data.forEach(function(item) {
+                      if(perms.convertPerms(item.permissions).MANAGE_GUILD == true) {
+                        guilds = guilds + sep + item.id + sepe + item.name
+                      }
+                    })
+                    guilds.split(sep).forEach(function(item) {
+                      if (item.split(sepe)[1] != undefined) {
+                        if (req.query.server == item.split(sepe)[0]) {name = item.split(sepe)[1]}
+                        servers.push(item.split(sepe)[0]) // bruh moment
+                      }
+                    });
+                  if (!servers.includes(req.query.server)) {
+                    res.send('Invalid Server/No Permission')
+                  } else {
+                    var invite = ""
+                    if (bot.guilds.get(req.query.server) ==! undefined) {
+                      invite = "You are good to go! Hatchverse is in this server!"
+                    } else {
+                      invite = "Hatchverse not found! Please invite Hatchverse to your server!"
+                    }
+                      const exists = await checkExists(req.query.server)
+                      if (exists == false) {
+                        db.get(`SELECT Language FROM Config WHERE Id = ${req.query.server}`, function (err,items) {
+                          console.log(items)
+                        res.render('dashboard', { name: name, current: items.Language, invite: invite});
+                        })
+                      } else {
+                        res.render('dashboard', { name: name, current: 'English', invite: invite});
+                      }
+                  }
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+  }
+})
+app.post('/save', function (req, res) {
+  console.log(req.query)
+  axios.get(`https://discordapp.com/api/v6/users/@me/guilds`, {
+                      headers: {
+                          "Authorization": `Bearer ${req.query.t}`,
+                          "Content-Type": "application/x-www-form-urlencoded" 
+                      }
+                  })
+                  .then(async (response) => {
+                    var guilds = ""
+                    var servers = []
+                    var name = ""
+                    response.data.forEach(function(item) {
+                      if(perms.convertPerms(item.permissions).MANAGE_GUILD == true) {
+                        guilds = guilds + sep + item.id + sepe + item.name
+                      }
+                    })
+                    guilds.split(sep).forEach(function(item) {
+                      if (item.split(sepe)[1] != undefined) {
+                        if (req.query.server == item.split(sepe)[0]) {name = item.split(sepe)[1]}
+                        servers.push(item.split(sepe)[0]) // bruh moment
+                      }
+                    });
+                  if (!servers.includes(req.query.s)) {
+                    return res.status(403).end('No Permission')
+                  } else {
+                      //make this async
+                      const exists = await checkExists(req.query.s);
+                      console.log(exists) // returns undefined + a
+                      if (exists == false) {
+                        db.run(`UPDATE Config SET Language = "${req.body.language}" WHERE Id = "${req.query.s}"`)
+                      } else {
+                        console.log(`INSERT INTO Config (Language, Id) VALUES (?,?)`,req.body.language, req.query.s)
+                        db.run(`INSERT INTO Config (Language, Id) VALUES (?,?)`,req.body.language, req.query.s)
+                      }
+                    return res.end('Saved!')
+                  }
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+})
