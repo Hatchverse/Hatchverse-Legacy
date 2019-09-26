00d416a9dd7e682d1bf697160fdcc7994cc319f4
Glitch (hatchverse) <none>
2019-09-26 00:05:44 +0000

    🍖🏩 Checkpoint ./commands/say.js:3089165/1078

---
+++ commands/say.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports.run = async (bot, message, args) => {
+  if(!message.content.startsWith(config.prefix)) return;
+  const devs = [
+    "302599378332549121",
+    "302527751745961985",
+    "198135885118570497",
+    "251123922005786624",
+    "374756186525794305"
+  ]
+  
+  if(!devs.includes(message.author.id)) return message.reply('no!');
+  message.delete()
+  message.channel.send(args.join(" "))
+}
+
+module.exports.help = {
+  name: "say"
+}
