270006d5ced7e678d955b44e8fba3626d1aedf49
Glitch (hatchverse) <none>
2019-11-18 03:10:44 +0000

    🚘💴 Checkpoint ./commands/say.js:3089165/126

--- commands/say.js
+++ commands/say.js
@@ -9,10 +9,10 @@ module.exports.run = async (bot, message, args) => {
     "198135885118570497",
     "251123922005786624",
     "374756186525794305",
-    "403259630437793804"
+    "403259630437793804","553601629887397888"
   ]
   
-  if(!devs.includes(message.author.id)) return message.reply('no!');
+  if(!devs.includes(message.author.id)) return message.channel.send('brain tumor diagnosis!?');
   message.delete()
   message.channel.send(args.join(" "))
 }
