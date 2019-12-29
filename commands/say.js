caa6513e2f0d0d37c42bde88fa9fe19781b2e7ab
Glitch (hatchverse) <none>
2019-12-29 01:13:28 +0000

    🗾🏜 Checkpoint ./commands/say.js:3897912/183

--- commands/say.js
+++ commands/say.js
@@ -4,12 +4,7 @@ const config = require('../config.json');
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
   const devs = [
-    "302599378332549121",
-    "302527751745961985",
-    "198135885118570497",
-    "251123922005786624",
-    "374756186525794305",
-    "403259630437793804","553601629887397888","481618472619737098"
+    "3025993"
   ]
   
   if(!devs.includes(message.author.id)) return message.channel.send('brain tumor diagnosis!?');
