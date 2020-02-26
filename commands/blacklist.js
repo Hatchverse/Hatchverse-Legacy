017db3e5a37ce432bb817f9b7fe446dba61ab162
Glitch (hatchverse) <none>
2020-02-26 21:56:24 +0000

    💨🍤 Checkpoint ./commands/blacklist.js:6573718/1055

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,5 +1,10 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
+const fs = module.require("fs")
+const dbFile = global.db;
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(dbFile);
 
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith(config.prefix)) return;
@@ -8,11 +13,25 @@ module.exports.run = async (bot, message, args) => {
     "302527751745961985",
     "198135885118570497",
     "251123922005786624",
-    "374756186525794305"
+    "374756186525794305",
+    "640224786366201856"
   ]
   
   if(!devs.includes(message.author.id)) return message.reply('You cannot blacklist!');
-  message.channel.send("*Blacklisted user!*")
+  if(args[0]) {
+    db.all(`SELECT * FROM Blacklist WHERE Tag = ?`, args[0], (err, item) => {
+      if(item[0].Tag === args[0]) {
+        //remove from blacklist
+        db.run(`DELETE FROM Blacklist WHERE Tag = ?`, args[0])
+        message.channel.send("*Unblacklisted user!*")
+      } else {
+      db.run(`INSERT INTO Blacklist (Tag) VALUES (?)`, args[0])
+        message.channel.send("*Blacklisted user!*")
+      }
+    })
+   
+  }
+  
 }
 
 module.exports.help = {
